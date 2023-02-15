a0c444ca5d6113af57e933e0900970b3a57ea960
richard200 <richmn200@gmail.com>
2023-02-15 14:08:32 +0300

    completed assignment

--- lib/song.rb
+++ lib/song.rb
@@ -10,30 +10,49 @@ class Song
           @artist = artist
           @genre = genre  
           @@count += 1
-          @@genres << genre unless @@genres.include?(genre)
-          @@artists << artist unless @@artists.include?(artist)
-            
+          @@genres << genre 
+          @@artists << artist 
           end
-    end
+  
 
     def self.count
         @@count
     end
 
-    def self.genres
-        @@genres
+    def self.artists
+        @@artists.uniq
     end
 
-    def self.artists
-        @@artists
-    end
+    def self.artist_count
+        artist_count = {}
+        @@artists.each do |artist|
+          if artist_count[artist]
+            artist_count[artist] += 1 
+          else
+            artist_count[artist] = 1
+          end
+        end
+        artist_count
+
+      end
+    
+      def self.genres
+        @@genres.uniq
+      end
+    
+      def self.genre_count
+        genre_count = {}
+        @@genres.each do |genre|
+          if genre_count[genre]
+            genre_count[genre] += 1 
+          else
+            genre_count[genre] = 1
+          end
+        end
+        genre_count
+    
+      
+      end
 
 end
 
-ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
-
-puts ninety_nine_problems.name
-
-puts ninety_nine_problems.artist
-
-puts ninety_nine_problems.genre
